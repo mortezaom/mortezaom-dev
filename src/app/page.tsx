99d3935063648654431e50eae80213605a4647dd
MortezaOM <mortezamohammady56@gmail.com>
2025-09-29 05:55:19 +0430

    a little responsive action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,23 +13,22 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
 	return (
 		<div className="flex flex-col justify-center items-center w-full h-full text-center">
-			<section className="gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr] mb-12">
+			<section className="gap-20 sm:gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr] mb-12">
 				<div className="flex flex-col items-start gap-4 text-start">
-					<h1 className="font-medium text-5xl xl:text-6xl">
+					<h1 className="font-medium text-4xl xl:text-6xl">
 						Hi, <br />
 						I'm{" "}
 						<span className="font-extrabold text-primary uppercase">
 							MortezaOM
 						</span>
 					</h1>
-					<p className="mt-3 font-semibold text-muted-foreground text-xl tracking-tight">
+					<p className="sm:my-3 font-semibold text-muted-foreground text-xl tracking-tight">
 						Full-Stack Developer
 					</p>
-					<br />
 
 					<ContactMeButton />
 				</div>
-				<div className="relative flex flex-col items-start gap-8 w-full text-start">
+				<div className="relative flex flex-col items-start gap-4 sm:gap-8 w-full text-start">
 					<h3 className="font-bold text-primary text-sm sm:text-base md:text-lg">
 						Expertise
 					</h3>
@@ -51,8 +50,8 @@ export default function Home() {
 					</div>
 				</div>
 			</section>
-			<div className="right-0 bottom-12 left-0 absolute flex mx-auto w-full">
-				<div className="flex flex-col justify-end items-start gap-3 mx-auto container">
+			<div className="right-0 bottom-12 left-0 absolute flex mx-auto p-4 w-full">
+				<div className="flex md:flex-col justify-end items-start gap-3 mx-auto container">
 					<Button
 						variant="ghost"
 						className="rounded-full size-11 cursor-pointer"
@@ -97,7 +96,7 @@ export default function Home() {
 					</Button>
 				</div>
 			</div>
-			<div className="top-1/2 left-1/2 -z-1 absolute w-[320px] sm:w-[420px] md:w-[560px] lg:w-[620px] xl:w-[680px] h-[320px] sm:h-[420px] md:h-[560px] lg:h-[620px] xl:h-[680px] -translate-x-1/2 -translate-y-1/2 transform">
+			<div className="top-1/2 -right-1/2 sm:left-1/2 -z-1 absolute w-[320px] sm:w-[420px] md:w-[560px] lg:w-[620px] xl:w-[680px] h-[320px] sm:h-[420px] md:h-[560px] lg:h-[620px] xl:h-[680px] -translate-x-1/2 -translate-y-1/2 transform">
 				<Image
 					src="/images/hero-image.webp"
 					alt=""
